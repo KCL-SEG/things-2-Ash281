282765f69d498034f821ae2741e08f123712d944
Ashvin Valentine <ashvin.valentine281@gmail.com>
2023-10-17 11:08:23 +0100

    Create thing form

--- things/tests.py
+++ things/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from things.models import Thing
+from things.forms import ThingForm
 
 # Create your tests here.
+class ThingFormTestCase(TestCase):
+    def setUp(self):
+        self.form_input = {
+            'name': "ash",
+            'quantity': "5",
+            'description': "hi",
+        }
+
+    # form acceps valid input data
+    def test_valid_form(self):    
+        form = ThingForm(data=self.form_input)
+        self.assertTrue(form.is_valid())
+        
+    # form users model validation
+    def test_form_uses_model_validation(self):
+        self.form_input['quantity'] = 'a' # remember usernames have @ in our app
+        form = ThingForm(data=self.form_input)
+        self.assertFalse(form.is_valid())
